4c28c5fe025137e1eab14ea0a1dc3258fc5b2ccd
Võ Văn Hiến <hienvvhien@gmail.com>
2026-03-20 10:31:30 +0700

    push code spring boot

---
+++ vovanhien/src/main/java/com/example/vovanhien/VovanhienApplication.java
@@ -0,0 +1,13 @@
+package com.example.vovanhien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VovanhienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VovanhienApplication.class, args);
+	}
+
+}
